124274e53ccdd41aab86f7c122b87d54cc837167
Gustavo Gomez <gustavogomez092@gmail.com>
2024-02-18 15:38:49 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Headless TGC
+
+## Description
+
+Headless TGC is a theme for the TGC App. This theme is designed to work with PHP 8.0 and requires at least WordPress version 4.7.
+
+## Version
+
+The current version of this theme is 0.1.0.
+
+## Author
+
+This theme was created by [TGC](https://tgc.com/).
+
+## Usage
+
+To use this theme, simply download it and follow the installation instructions for your WordPress installation.
+
+## Contributing
+
+If you wish to contribute to this project, please contact the author.
+
+
+## Contact
+
+For more information, please visit the [TGC website](https://tgc.com/).
---
+++ functions.php
@@ -0,0 +1,83 @@
+<?php
+require_once('plugin-activation/activate-plugins.php');
+/**
+ * Headless functions and definitions
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
+
+// add featured images to posts
+add_theme_support('post-thumbnails');
+
+
+// add custom categories at theme activation
+add_action( 'after_setup_theme', 'custom_add_cat' );
+
+function custom_add_cat() {
+
+    wp_insert_term(
+        'Code', 
+        'category', 
+        array('slug' => 'code')
+    );
+
+    wp_insert_term(
+        'Tech', 
+        'category', 
+        array('slug' => 'tech')
+    );
+
+    wp_insert_term(
+        'Gaming', 
+        'category', 
+        array('slug' => 'gaming')
+    );
+
+}
+
+
+// WPGrahQL resolver for the lastest posts
+
+register_graphql_object_type( 'Notification', [
+    'description' => __( 'notifications object', 'tgc' ),
+    'fields'      => [
+        'title'   => [
+            'type' => 'String',
+        ],
+        'excerpt' => [
+            'type' => 'String',
+        ],
+        'date'    => [
+            'type' => 'String',
+        ],
+        'image'  => [
+            'type' => 'String',
+        ],
+    ]
+] );
+
+register_graphql_field('RootQuery', 'notification_center', [
+    'type' => ['list_of' => 'Notification'],
+    'description' => 'The latest posts for the notification center.',
+    'resolve' => function ($source, $args, $context, $info) {
+        $query_args = [
+            'post_type' => 'post',
+            'posts_per_page' => 10,
+        ];
+        $query = new WP_Query($query_args);
+
+        $data = array();
+        foreach ($query->posts as $post) {
+            $data[] = [
+                'title'   => $post->post_title,
+                'excerpt' => $post->post_excerpt,
+                'date'    => $post->post_date,
+                'image'   => get_the_post_thumbnail_url($post->ID),
+            ];
+        }
+
+        return $data;
+    },
+]);
