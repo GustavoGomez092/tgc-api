5dcb809e3a1b1fdcfd60662e6cc0b7728dea0562
Gustavo Gomez <gustavogomez092@gmail.com>
2024-03-07 09:49:39 -0600

    GQL stuff moved to its own file

--- functions.php
+++ functions.php
@@ -11,73 +11,74 @@ if (!defined('ABSPATH')) {
 // add featured images to posts
 add_theme_support('post-thumbnails');
 
+if ( in_array( 'wp-graphql/wp-graphql.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    // add custom categories at theme activation
+    add_action( 'after_setup_theme', 'custom_add_cat' );
 
-// add custom categories at theme activation
-add_action( 'after_setup_theme', 'custom_add_cat' );
+    function custom_add_cat() {
 
-function custom_add_cat() {
+        wp_insert_term(
+            'Code', 
+            'category', 
+            array('slug' => 'code')
+        );
 
-    wp_insert_term(
-        'Code', 
-        'category', 
-        array('slug' => 'code')
-    );
+        wp_insert_term(
+            'Tech', 
+            'category', 
+            array('slug' => 'tech')
+        );
 
-    wp_insert_term(
-        'Tech', 
-        'category', 
-        array('slug' => 'tech')
-    );
+        wp_insert_term(
+            'Gaming', 
+            'category', 
+            array('slug' => 'gaming')
+        );
 
-    wp_insert_term(
-        'Gaming', 
-        'category', 
-        array('slug' => 'gaming')
-    );
-
-}
+    }
 
 
-// WPGrahQL resolver for the lastest posts
+    // WPGrahQL resolver for the lastest posts
 
-register_graphql_object_type( 'Notification', [
-    'description' => __( 'notifications object', 'tgc' ),
-    'fields'      => [
-        'title'   => [
-            'type' => 'String',
-        ],
-        'excerpt' => [
-            'type' => 'String',
-        ],
-        'date'    => [
-            'type' => 'String',
-        ],
-        'image'  => [
-            'type' => 'String',
-        ],
-    ]
-] );
+    register_graphql_object_type( 'Notification', [
+        'description' => __( 'notifications object', 'tgc' ),
+        'fields'      => [
+            'title'   => [
+                'type' => 'String',
+            ],
+            'excerpt' => [
+                'type' => 'String',
+            ],
+            'date'    => [
+                'type' => 'String',
+            ],
+            'image'  => [
+                'type' => 'String',
+            ],
+        ]
+    ] );
 
-register_graphql_field('RootQuery', 'notification_center', [
-    'type' => ['list_of' => 'Notification'],
-    'description' => 'The latest posts for the notification center.',
-    'resolve' => function ($source, $args, $context, $info) {
-        $query_args = [
-            'post_type' => 'post',
-            'posts_per_page' => 10,
-        ];
-        $query = new WP_Query($query_args);
-
-        $data = array();
-        foreach ($query->posts as $post) {
-            $data[] = [
-                'title'   => $post->post_title,
-                'excerpt' => $post->post_excerpt,
-                'date'    => $post->post_date,
-                'image'   => get_the_post_thumbnail_url($post->ID),
+    register_graphql_field('RootQuery', 'notification_center', [
+        'type' => ['list_of' => 'Notification'],
+        'description' => 'The latest posts for the notification center.',
+        'resolve' => function ($source, $args, $context, $info) {
+            $query_args = [
+                'post_type' => 'post',
+                'posts_per_page' => 10,
             ];
-        }
+            $query = new WP_Query($query_args);
 
-        return $data;
-    },
-]);
+            $data = array();
+            foreach ($query->posts as $post) {
+                $data[] = [
+                    'title'   => $post->post_title,
+                    'excerpt' => $post->post_excerpt,
+                    'date'    => $post->post_date,
+                    'image'   => get_the_post_thumbnail_url($post->ID),
+                ];
+            }
+
+            return $data;
+        },
+    ]);
+}
