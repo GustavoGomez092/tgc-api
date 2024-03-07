739121ec7c4936916a6fece23b2158173d33bd4e
Gustavo Gomez <gustavogomez092@gmail.com>
2024-03-07 12:49:58 -0600

    :fire: added featured post flag that can only be on one post at a time + added where clause to posts query to get the featured post

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 require_once('plugin-activation/activate-plugins.php');
-if ( is_plugin_active('wp-graphql/wp-graphql.php') ) {
+if (is_plugin_active('wp-graphql/wp-graphql.php')) {
     require_once('graphql.php');
 }
 /**
@@ -16,26 +16,106 @@ add_theme_support('post-thumbnails');
 
 
 // add custom categories at theme activation
-add_action( 'after_setup_theme', 'custom_add_cat' );
+add_action('after_setup_theme', 'custom_add_cat');
 
-function custom_add_cat() {
+function custom_add_cat()
+{
 
     wp_insert_term(
-        'Code', 
-        'category', 
+        'Code',
+        'category',
         array('slug' => 'code')
     );
 
     wp_insert_term(
-        'Tech', 
-        'category', 
+        'Tech',
+        'category',
         array('slug' => 'tech')
     );
 
     wp_insert_term(
-        'Gaming', 
-        'category', 
+        'Gaming',
+        'category',
         array('slug' => 'gaming')
     );
 
 }
+
+/*
+ * Adds a Featured Post meta box to the post editing screen
+ */
+function blog_featured_meta()
+{
+    add_meta_box('blog_meta', __('Featured Post', 'blog-textdomain'), 'blog_meta_callback', 'post', 'side', 'high');
+}
+add_action('add_meta_boxes', 'blog_featured_meta');
+
+/**
+ * Outputs the content of the meta box
+ */
+
+function blog_meta_callback($post)
+{
+    wp_nonce_field(basename(__FILE__), 'blog_nonce');
+    $blog_stored_meta = get_post_meta($post->ID);
+    ?>
+
+<p>
+  <span class="blog-row-title">
+    <?php _e('Check if this is a featured post: <br/> <span style="color: red;">Only one post can be featured at a time.</span>', 'blog-textdomain') ?>
+  </span>
+<div class="blog-row-content">
+  <label for="featured-checkbox">
+    <input type="checkbox" name="featured-checkbox" id="featured-checkbox" value="yes" <?php if (isset($blog_stored_meta['featured-checkbox']))
+                checked($blog_stored_meta['featured-checkbox'][0], 'yes'); ?> />
+    <?php _e('Featured Item', 'blog-textdomain') ?>
+  </label>
+
+</div>
+</p>
+
+<?php
+}
+
+/**
+ * Saves the custom meta input
+ */
+function blog_meta_save($post_id)
+{
+
+    // Checks save status - overcome autosave, etc.
+    $is_autosave = wp_is_post_autosave($post_id);
+    $is_revision = wp_is_post_revision($post_id);
+    $is_valid_nonce = (isset($_POST['blog_nonce']) && wp_verify_nonce($_POST['blog_nonce'], basename(__FILE__))) ? 'true' : 'false';
+
+    // Exits script depending on save status
+    if ($is_autosave || $is_revision || !$is_valid_nonce) {
+        return;
+    }
+
+    // Checks for input and saves - save checked as yes and unchecked at no
+    if (isset($_POST['featured-checkbox'])) {
+        // unchecks all featured psots before saving the new one
+        $args = array(
+            'post_type' => 'post',
+            'meta_query' => array(
+                array(
+                    'key' => 'featured-checkbox',
+                    'value' => 'yes',
+                ),
+            ),
+        );
+        $query = new WP_Query($args);
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                update_post_meta(get_the_ID(), 'featured-checkbox', 'no');
+            }
+        }
+        update_post_meta($post_id, 'featured-checkbox', 'yes');
+    } else {
+        update_post_meta($post_id, 'featured-checkbox', 'no');
+    }
+
+}
+add_action('save_post', 'blog_meta_save');
--- graphql.php
+++ graphql.php
@@ -51,3 +51,74 @@ register_graphql_field('RootQuery', 'notification_center', [
         return $data;
     },
 ]);
+
+// register the is featured meta to posts type
+
+add_action('graphql_register_types', function () {
+    register_graphql_field('Post', 'featured-post', [
+        'type' => 'Boolean',
+        'description' => __('Flag to know if the post featured', 'wp-graphql'),
+        'resolve' => function ($post) {
+            $featured = get_post_meta($post->ID, 'featured-checkbox', true);
+            return !empty ($featured) && $featured === 'yes' ? true : false;
+        }
+    ]);
+});
+
+// add new query to get the featured post
+
+// register_graphql_field('RootQuery', 'featuredPost', [
+//     'type' => 'Post',
+//     'description' => 'Get the featured post of the site.',
+//     'resolve' => function ($source, $args, $context, $info) {
+//         $args = array (
+//             'post_type' => 'post',
+//             'meta_query' => array (
+//                 array (
+//                     'key' => 'featured-checkbox',
+//                     'value' => 'yes',
+//                 ),
+//             ),
+//         );
+//         $post = null;
+//         $query = new WP_Query($args);
+//         if ($query->have_posts()) {
+//             while ($query->have_posts()) {
+//                 $query->the_post();
+//                 $post = $query->post;
+//             }
+//         }
+//         return $post;
+//     },
+// ]);
+
+
+
+// First, we register the field in the "where" clause.
+add_action('graphql_register_types', function () {
+
+    $customposttype_graphql_single_name = "Post"; // Replace this with your custom post type single name in PascalCase
+
+    // Registering the 'featured' argument in the 'where' clause.
+    register_graphql_field('RootQueryTo' . $customposttype_graphql_single_name . 'ConnectionWhereArgs', 'featured', [
+        'type' => 'Boolean', // To accept boolean values
+        'description' => __('Filter by post objects by featured flag', 'wp-graphql'),
+    ]);
+});
+
+// Next, we add a filter to modify the query arguments.
+add_filter('graphql_post_object_connection_query_args', function ($query_args, $source, $args, $context, $info) {
+
+    $featured = $args['where']['featured']; // Accessing the 'featured' argument.
+
+    if (isset ($featured) && $featured === true) {
+        $query_args['meta_query'] = [
+            [
+                'key' => 'featured-checkbox',
+                'value' => 'yes',
+            ],
+        ];
+    }
+
+    return $query_args;
+}, 10, 5);
